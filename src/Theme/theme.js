040a49981b1d351b87a0d75e893006c850a8d535
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2022-05-22 16:22:52 +0200

    add themes

---
+++ src/Theme/theme.js
@@ -0,0 +1,27 @@
+import { widths } from "./widths";
+import { colors } from "./colors";
+import { fonts } from "./fonts";
+import { devices } from "./devices";
+
+export const lightTheme = {
+  widths,
+  colors,
+  fonts,
+  devices,
+  body: "#ffffff",
+  text: "#1a1a1a",
+  toggleBorder: "#0f2b5c",
+  background: "#161616",
+  afterBG: "#759eb37d",
+};
+export const darkTheme = {
+  widths,
+  colors,
+  fonts,
+  devices,
+  body: "#121112",
+  text: "#ffffff",
+  toggleBorder: "#2177d2",
+  background: "#171717",
+  afterBG: "rgba(0,0,0,0.5)",
+};
